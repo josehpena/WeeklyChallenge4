e8e361e998adb587b27ed843119a3808137b08ca
Jose Pena <josehpena2@gmail.com>
2021-04-29 13:24:00 -0300

    Tests Working

--- challenge/src/test/java/com/ifood/challenge/ChallengeApplicationTests.java
+++ challenge/src/test/java/com/ifood/challenge/ChallengeApplicationTests.java
@@ -1,13 +1,64 @@
 package com.ifood.challenge;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ifood.challenge.location.LocationController;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ChallengeApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private final String BASE_URL = "/feather/";
+
+	//Instância do ObjectMapper para trabalhar com JSON
+	private ObjectMapper objectMapper;
+
+	//Controlador REST tratado por meio de injeção de dependências
+	@Autowired
+	private LocationController restController;
+
+	//Instância do MockMVC
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		objectMapper = new ObjectMapper();
+		mockMvc = MockMvcBuilders
+				.standaloneSetup(restController)
+				.build();
 	}
+
+	@Test
+	public void not_found_test_404() throws Exception {
+		mockMvc.perform(get(BASE_URL)).andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void createLocationTest() throws Exception{
+
+		ResultActions resultActions = mockMvc.perform(
+				get("/feather/1")
+				.contentType(MediaType.APPLICATION_JSON));
+
+			resultActions
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$[0].name").value("Location 0"));
+	}
+
 
 }
